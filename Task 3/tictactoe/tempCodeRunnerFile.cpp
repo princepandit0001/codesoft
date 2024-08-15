75e5fc86227eaeb65ba950c7934303d9117d4f51
Prince Sharma <137486688+princepandit0001@users.noreply.github.com>
2024-08-15 22:37:22 +0530

    Add files via upload

---
+++ Task 3/tictactoe/tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+ char board[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+
+    cout<<"\n welcome to tic-tac-toe game  \n";
+    cout<<"  \t Player 1 [x]  \n \t Player 2[0]  \n"; 
+
+    cout<<"\t\t     |     |     \n";
+    cout<<"\t\t  "<<board[0][0]<<"  |  "<<board[0][1]<<"  |  "<<board[0][2]<<"  \n";
+    cout<<"\t\t_____|_____|_____\n";
+    cout<<"\t\t     |     |     \n";
+    cout<<"\t\t  "<<board[1][0]<<"  |  "<<board[1][1]<<"  |  "<<board[1][2]<<"  \n";
+    cout<<"\t\t_____|_____|_____\n";
+    cout<<"\t\t     |     |     \n";
+    cout<<"\t\t  "<<board[2][0]<<"  |  "<<board[2][1]<<"  |  "<<board[2][2]<<"  \n";
+    cout<<"\t\t     |     |     \n";
+    cout<<" \n\t Player 1 [x]  turn \n";
